c32e78b9b6e7dc0cab2fb489d8c6e6bde7554210
praneeth27 <30894084+praneeth27@users.noreply.github.com>
2017-09-28 14:59:56 +0530

    Update crypto-suite.py

--- crypto-suite.py
+++ crypto-suite.py
@@ -20,21 +20,6 @@ def modulo_exp(a,b,c):
 		else :
 			return ((modulo_exp(a,b-1,c))*(a%c))%c
 
-def discrete_logarithm(a,b,n):
-	m = int(math.ceil(math.sqrt(n)))
-	dict = {}
-	for j in range(m):
-		dict.update({j:pow(a,j,n)})
-	r = invmod(a,n)**m	
-	y = b	
-	#print (dict['j'])
-	for i in range(m):
-		for k in range(m):
-			if(dict[k] == y):
-				return i*m + k
-			else:
-				pass
-		y = modulo_exp(y,r,n)
 def extgcd(a,b):
     if a==0:
         return (b,0,1)
